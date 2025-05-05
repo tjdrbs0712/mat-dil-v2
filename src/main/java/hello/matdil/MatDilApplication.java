08e194ca9c3a50e031647ffec46e323c6c2a24cd
seongkyun <yooss135@naver.com>
2025-05-05 11:17:46 +0900

    git init

---
+++ src/main/java/hello/matdil/MatDilApplication.java
@@ -0,0 +1,13 @@
+package hello.matdil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatDilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatDilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/matdil/MatDilApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.matdil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatDilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
