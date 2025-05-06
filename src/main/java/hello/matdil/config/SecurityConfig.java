b719bd03250f818b027505f3e3c711d51487f081
seongkyun <yooss135@naver.com>
2025-05-07 02:50:28 +0900

    feat: SecurityConfig 설정

--- src/main/java/hello/matdil/config/SecurityConfig.java
+++ src/main/java/hello/matdil/config/SecurityConfig.java
@@ -2,14 +2,37 @@ package hello.matdil.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
+                        ).permitAll()
+                        .requestMatchers("/users/register").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults()) // 기본 인증 UI 사용 (필요 시 삭제)
+                .csrf(AbstractHttpConfigurer::disable);       // CSRF 비활성화
+        return http.build();
+    }
+
 }
