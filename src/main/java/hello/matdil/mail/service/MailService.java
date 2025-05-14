f83728664ec4842a6c7f71af7c050fa3ef1d8473
seongkyun <yooss135@naver.com>
2025-05-15 03:38:38 +0900

    fix: 이메일 인증 url 수정

--- src/main/java/hello/matdil/mail/service/MailService.java
+++ src/main/java/hello/matdil/mail/service/MailService.java
@@ -14,7 +14,7 @@ public class MailService {
     public void sendEmailVerification(String to, String token) {
         String subject = "이메일 인증 링크입니다.";
         String text = "다음 링크를 클릭하여 인증을 완료해주세요:\n\n"
-                + "http://localhost:8080/api/users/verify-email?token=" + token;
+                + "http://localhost:8080/api/auth/verify-email?token=" + token;
 
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
