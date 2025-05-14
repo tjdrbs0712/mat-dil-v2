650dbaf6fbba2479759275546bb5a22610afb099
seongkyun <yooss135@naver.com>
2025-05-15 03:18:06 +0900

    feature: db에서 id, email로 active 계정만 가져오기 추기

--- src/main/java/hello/matdil/domain/user/repository/UserRepository.java
+++ src/main/java/hello/matdil/domain/user/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package hello.matdil.domain.user.repository;
 
 import hello.matdil.domain.user.entity.User;
+import hello.matdil.domain.user.entity.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     boolean existsByPhoneNumber(String phoneNumber);
     Optional<User> findByEmail(String email);
+    Optional<User> findByIdAndUserStatus(Long id, UserStatus status);
+    Optional<User> findByEmailAndUserStatus(String email, UserStatus status);
 }
