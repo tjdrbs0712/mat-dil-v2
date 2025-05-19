7084542a58bf2fb1fb2548378cb7c08234ed9cbf
seongkyun <yooss135@naver.com>
2025-05-20 02:59:02 +0900

    refactor: 최소 금액, 배달시간 설정

--- src/main/java/hello/matdil/domain/store/dto/StoreCreateRequestDto.java
+++ src/main/java/hello/matdil/domain/store/dto/StoreCreateRequestDto.java
@@ -1,5 +1,6 @@
 package hello.matdil.domain.store.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -33,8 +34,10 @@ public class StoreCreateRequestDto {
     private LocalTime closeTime;
 
     @NotNull(message = "최소 주문 금액은 필수입니다.")
+    @Min(value = 0, message = "최소 주문 금액은 0원 이상이어야 합니다.")
     private Integer minOrderPrice;
 
     @NotNull(message = "배달 예상 시간은 필수입니다.")
+    @Min(value = 0, message = "최소 배달 시간은 0분 이상이어야 합니다.")
     private Integer deliveryTimeEstimate;
 }
