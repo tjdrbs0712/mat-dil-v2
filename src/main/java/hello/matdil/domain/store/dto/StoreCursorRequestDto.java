25e1d4835f83e895d58be280f10a574fa5c9e3ef
seongkyun <yooss135@naver.com>
2025-05-20 05:53:10 +0900

    refactor: 패키지 위치 클래스명 변경

--- src/main/java/hello/matdil/domain/store/dto/StoreCursorRequestDto.java
+++ src/main/java/hello/matdil/domain/store/dto/StoreCursorRequestDto.java
@@ -1,11 +1,11 @@
-package hello.matdil.global.response;
+package hello.matdil.domain.store.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor(staticName = "of")
-public class Cursor {
+public class StoreCursorRequestDto {
     private Object value;     // lastRating, lastName 등
     private Long storeId;     // tie-breaker
 }
