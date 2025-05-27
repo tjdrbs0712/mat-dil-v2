e80e10131116d26aa076d89c3ad81a1516f5da07
seongkyun <yooss135@naver.com>
2025-05-27 13:08:38 +0900

    feature: kafka 토픽 생성

---
+++ src/main/java/hello/matdil/config/KafkaTopicConfig.java
@@ -0,0 +1,34 @@
+package hello.matdil.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic orderTopic() {
+        return TopicBuilder.name("order-topic")
+                .partitions(5)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic notificationTopic() {
+        return TopicBuilder.name("notification-topic")
+                .partitions(5)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic paymentTopic() {
+        return TopicBuilder.name("payment-topic")
+                .partitions(5)
+                .replicas(1)
+                .build();
+    }
+}
