af253ea14caa7503a6996f6d539b5f2b6a1a551e
seongkyun <yooss135@naver.com>
2025-06-11 11:36:19 +0900

    refactor: kafka consumer에서 발생한 에러는 throw가 아니라 log로 관리

--- src/main/java/hello/matdil/domain/delivery/service/DeliveryService.java
+++ src/main/java/hello/matdil/domain/delivery/service/DeliveryService.java
@@ -2,17 +2,17 @@ package hello.matdil.domain.delivery.service;
 
 import hello.matdil.domain.delivery.entity.Delivery;
 import hello.matdil.domain.delivery.entity.DeliveryStatus;
-import hello.matdil.domain.delivery.exception.DeliveryErrorCode;
-import hello.matdil.domain.delivery.exception.DeliveryException;
 import hello.matdil.domain.delivery.repository.DeliveryRepository;
 import hello.matdil.domain.order.event.OrderReadyForDispatchEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class DeliveryService {
 
     private final DeliveryRepository deliveryRepository;
@@ -21,7 +21,8 @@ public class DeliveryService {
     @Transactional
     public void createDelivery(OrderReadyForDispatchEvent event) {
         if (deliveryRepository.existsByOrderId(event.orderId())) {
-            throw new DeliveryException(DeliveryErrorCode.DELIVERY_ALREADY_EXISTS);
+            log.warn("이미 배달이 생성된 주문입니다. orderId={}", event.orderId());
+            return;
         }
 
         Delivery newDelivery = Delivery.create(
